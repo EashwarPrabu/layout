dee72a1be8c15edb646c2c8ee7ef1c0bb73168bb
EashwarPrabu <eashwarpc@gmail.com>
2020-04-19 02:01:54 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.teal[800],
+      body: SafeArea(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.red,
+            ),
+            SizedBox(width: 50.0),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.green,
+                )
+              ],
+            ),
+            SizedBox(width: 50.0),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
